2d5e9b472df26f480f1e994d90152d466b16fd0b
Franco Prudhomme <francoprud@gmail.com>
2016-08-22 18:43:11 -0300

    Controlling the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        rb2d.AddForce(new Vector2(moveHorizontal, moveVertical)  * speed);
+	}
+}
